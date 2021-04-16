c8c92f7a3322fa48442d352023e5540fa75b0939
Oyama <Oyama@100.64.1.154>
2021-04-17 05:17:01 +0900

    first

---
+++ coaraMania/src/main/java/com/example/demo/service/CoaraService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entitiy.Coara;
+
+
+public interface CoaraService {
+	
+	List<Coara> getAll();
+
+}
---
+++ coaraMania/src/main/java/com/example/demo/service/CoaraServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Coara;
+import com.example.demo.repository.CoaraDao;
+
+@Service
+public class CoaraServiceImpl implements CoaraService{
+
+	private final CoaraDao dao;
+	
+	@Autowired
+	public CoaraServiceImpl(CoaraDao dao) {
+		this.dao = dao;
+	}
+	
+	@Override
+	public void save(Coara coara) {
+		dao.insertCoara(coara);
+	}
+
+	
+//  This method is used in the latter chapter
+//	@Override
+//	public void update(Inquiry inquiry) {
+//		
+//		//return dao.updateInquiry(inquiry);
+//		if(dao.updateInquiry(inquiry) == 0) {
+//			throw new InquiryNotFoundException("can't find the same ID");
+//		}
+//	}
+	
+	@Override
+	public List<Coara> getAll() {
+		return dao.getAll();
+	}
+}
