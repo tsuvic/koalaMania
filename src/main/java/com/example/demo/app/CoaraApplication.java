d3e0ee8090aa5258ef534082d1b1eec2987cdcf5
Oyama <Oyama@100.64.1.154>
2021-04-18 22:02:59 +0900

    main calss added!

---
+++ src/main/java/com/example/demo/app/CoaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoaraApplication.class, args);
+	}
+
+}
