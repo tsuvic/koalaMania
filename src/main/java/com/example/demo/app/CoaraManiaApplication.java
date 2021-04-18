e25f488cf28c49d1af1b2a073c268916c38518b3
Oyama <Oyama@100.64.1.154>
2021-04-18 22:32:40 +0900

    yoshi's bug fix分反映

--- src/main/java/com/example/demo/app/CoaraManiaApplication.java
+++ src/main/java/com/example/demo/app/CoaraManiaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoaraApplication {
+public class CoaraManiaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoaraApplication.class, args);
+		SpringApplication.run(CoaraManiaApplication.class, args);
 	}
 
 }
