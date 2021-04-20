2a52e6624b2af59693601d4b649798a109b9fcff
Oyama <Oyama@100.64.1.154>
2021-04-20 14:14:52 +0900

    SQL for schema and data updated!

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO coara(name,sex)
-VALUES('Tarou', 1);
-INSERT INTO coara(name,sex)
-VALUES('Hanako', 0);
-INSERT INTO coara(name,sex)
-VALUES('Kazuo', 1);
+INSERT INTO coara(name, is_male, birthdate, is_alive, zoo, mother, father, details, feature)
+VALUES('ひろし', 1, '9999-01-01', 1, '多摩動物公園', 'つる', '銀の介', '子供たちにはいわゆる「褒めて伸ばす」接し方が良いというスタンスらしく、みさえとは教育方針で対立することがある', '男前');
+INSERT INTO coara(name, is_male, birthdate, is_alive, zoo, mother, father, details, feature)
+VALUES('みさえ', 1, '1963-10-10', 0, '多摩動物公園', 'ひさえ', 'よし治', '時に個性的、時に母性的であるが、基本は“短気”と“見栄っ張り”と“ケチ”の三拍子を備えた利かん気で子供染みた女性である。', '男勝り');
+INSERT INTO coara(name, is_male, birthdate, is_alive, zoo, mother, father, details, feature)
+VALUES('しんのすけ', 1, '1987-05-05', 1, '多摩動物公園', 'みさえ', 'ひろし', '元気いっぱいのコアラです。', 'おもろい');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,14 @@
 CREATE TABLE coara
 (
-   id INT NOT NULL AUTO_INCREMENT,
+   coara_id INT NOT NULL AUTO_INCREMENT,
    name VARCHAR(100) NOT NULL,
-   sex INT(1) NOT NULL,
-   PRIMARY KEY(id)
+   is_male INT(1) NOT NULL,
+   birthdate DATE,
+   is_alive INT(1) NOT NULL,
+   zoo VARCHAR(50) NOT NULL,
+   mother VARCHAR(50) NOT NULL,
+   father VARCHAR(50) NOT NULL,
+   details VARCHAR(300) NOT NULL,
+   feature VARCHAR(50) NOT NULL,
+   PRIMARY KEY(coara_id)
 );
