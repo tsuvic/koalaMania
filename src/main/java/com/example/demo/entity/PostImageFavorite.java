e3c769f0b95717ccb68ba8ae9d6d686e6c3a25b6
tsuvi <admja11@gmail.com>
2022-06-25 10:36:54 +0900

    ユーザーページ既存機能の削除

--- src/main/java/com/example/demo/entity/PostImageFavorite.java
+++ src/main/java/com/example/demo/entity/PostImageFavorite.java
@@ -1,24 +1,18 @@
 package com.example.demo.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.springframework.stereotype.Component;
-
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 @Data
 @Component
 public class PostImageFavorite {
 
-	@JsonIgnore
 	public final String TABLE_NAME = "post_image_favorite";
 
-	@JsonIgnore
 	public final String COLUMN_POST_IMAGE_FAVORITE_ID = "postimagefavorite_id";
 
-	@JsonIgnore
 	public final String COLUMN_POST_IMAGE_ID = "postimage_id";
 
-	@JsonIgnore
 	public final String COLUMN_USER_ID = "user_id";
 
 	private int postimageFavoriteId;
