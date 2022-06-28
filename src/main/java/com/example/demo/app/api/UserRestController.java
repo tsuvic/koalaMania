3c652e90ba835ffc100b05d252124f322c7acf76
tsuvi <admja11@gmail.com>
2022-06-29 00:00:45 +0900

    バックエンド：Spring Security のCORS, CSRFの設定 フロントエンド：axiosのヘッダー設定

--- src/main/java/com/example/demo/app/api/UserRestController.java
+++ src/main/java/com/example/demo/app/api/UserRestController.java
@@ -7,6 +7,7 @@ import com.example.demo.entity.LoginUser;
 import com.example.demo.entity.Post;
 import com.example.demo.service.PostService;
 import com.example.demo.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,84 +16,67 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api/users")
 public class UserRestController {
 	private final UserAuthenticationUtil userAuthenticationUtil;
 	private final UserService userService;
 	private final PostService postService;
+	static ObjectMapper objectMapper = new ObjectMapper();
 
 	@Autowired
-	public UserRestController(UserAuthenticationUtil userAuthenticationUtil,
-							  UserService userService,
-							  PostService postService) {
+	public UserRestController(UserAuthenticationUtil userAuthenticationUtil, UserService userService, PostService postService) {
 		this.userAuthenticationUtil = userAuthenticationUtil;
 		this.userService = userService;
 		this.postService = postService;
 	}
 
+	/**
+	 *　Spring Securityにて認証有無を確認する
+	 * @param form ユーザーフォーム
+	 * @return 認証済みの場合、ユーザー情報
+	 * @throws Exception
+	 */
 	@RequestMapping("/checkAuthenticated")
-	@CrossOrigin(
-			origins = {"http://127.0.0.1:8080","http://localhost:8080","https://koalamania.herokuapp.com"},
-			allowCredentials = "true"
-	)
-	String checkAuthenticated(@ModelAttribute UserForm form) throws Exception {
+	String checkAuthenticated(@ModelAttribute UserForm form) throws JsonProcessingException {
 		LoginUser loginUser = userAuthenticationUtil.isUserAuthenticated();
 		if (loginUser != null) {
-			form.setUser_id(loginUser.getUser_id());
-			form.setName(loginUser.getUserName());
-			form.setProfile(loginUser.getProfile());
-			form.setProfileImagePath(loginUser.getProfileImagePath());
-			form.setTwitterLinkFlag(loginUser.isTwitterLinkFlag());
-			System.out.println("HI");
-			return new ObjectMapper().writeValueAsString(form);
+			return objectMapper.writeValueAsString(loginUser);
 		} else {
-			System.out.println("null");
 			return null;
 		}
 	}
 
-
 	@GetMapping("/{userId}")
-	@CrossOrigin(
-			origins = {"http://127.0.0.1:8080","http://localhost:8080","https://koalamania.herokuapp.com"},
-			allowCredentials = "true"
-	)
-	String getUser(@PathVariable int userId, @ModelAttribute UserForm form, Model model) throws Exception {
-		LoginUser loginUser = userAuthenticationUtil.isUserAuthenticated();
-		if (loginUser != null && loginUser.getUser_id() == userId) {
-			form.setUser_id(userId);
-			form.setName(loginUser.getUserName());
-			form.setProfile(loginUser.getProfile());
-			form.setTwitterLinkFlag(loginUser.isTwitterLinkFlag());
-			form.setAdress(loginUser.getProvider_adress());
-			form.setProfileImagePath(loginUser.getProfileImagePath());
+	String getUser(@PathVariable int userId, @ModelAttribute UserForm form, Model model) throws JsonProcessingException {
+		LoginUser user = userAuthenticationUtil.isUserAuthenticated();
+		if (user != null && user.getUser_id() == userId) {
 			model.addAttribute("editFlag", true);
 		} else {
-			LoginUser otherUser = userService.findById(userId);
-			form.setName(otherUser.getUserName());
-			form.setProfile(otherUser.getProfile());
-			form.setTwitterLinkFlag(otherUser.isTwitterLinkFlag());
-			form.setAdress(otherUser.getProvider_adress());
-			form.setProfileImagePath(otherUser.getProfileImagePath());
+			user = userService.findById(userId);
 			model.addAttribute("editFlag", false);
 		}
 
-		if (form.getProfileImagePath() == null) {
-			form.setProfileImagePath("/images/users/profile/defaultUser.png");
+		if (user.getProfileImagePath() == null) {
+			user.setProfileImagePath("/images/users/profile/defaultUser.png");
 		}
-		return new ObjectMapper().writeValueAsString(form);
+		return objectMapper.writeValueAsString(user);
 	}
 
 	@GetMapping("/{userId}/posts")
-	@CrossOrigin(
-			origins = {"http://127.0.0.1:8080","http://localhost:8080","https://koalamania.herokuapp.com"},
-			allowCredentials = "true"
-	)
-	String getPosts(@PathVariable int userId, @ModelAttribute UserForm form, Model model) throws Exception {
+	String getPosts(@PathVariable int userId) throws JsonProcessingException {
 		List<Post> postList = postService.getPostByUserId(userId);
-		ObjectMapper objectMapper = new ObjectMapper();
 		objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
 		return objectMapper.writeValueAsString(postList);
+	}
+
+	@PostMapping("/{userId}/posts")
+	String postPosts(@PathVariable int userId, @ModelAttribute Post post) throws JsonProcessingException {
+
+		System.out.println(userId);
+		System.out.println(post);
+		System.out.println(post.getPost_id());
+		return null;
 	}
 }
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,50 +1,62 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-	@Configuration
-	public static class AppSecurityConfigureAdapter extends WebSecurityConfigurerAdapter {
-
-		@Override
-		public void configure(WebSecurity web) throws Exception {
-			web.ignoring().antMatchers("/css/**", "/js/**", "/fonts/**", "/img/**");
-		}
-
-		protected void configure(HttpSecurity http) throws Exception {
-			http
-				.authorizeRequests()
-					.antMatchers("/").permitAll()
-					.antMatchers("/insert").authenticated()
-					.antMatchers("/edit/*").authenticated()
-					.antMatchers("/post/postInsert").authenticated()
-					.antMatchers("/delete/*").authenticated()
-					.antMatchers("/api/users/checkAuthenticated").authenticated()
-
-			.and()
-					.exceptionHandling()
-
-			.and()
-				.formLogin()
-				.loginProcessingUrl("/oauth/twitter/access")
-				.loginPage("/login")
-
-			.and()
-			.logout()
-			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-			.logoutUrl("/logout")
-			.logoutSuccessUrl("/")
-			.deleteCookies("JSESSIONID").invalidateHttpSession(true)
-			.deleteCookies("autoLogin");
-		}
-
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**", "/js/**", "/fonts/**", "/img/**");
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors(cors -> cors
+				.configurationSource(corsConfigurationSource())
+			).authorizeRequests(authorize -> authorize
+				.antMatchers("/").permitAll()
+				.antMatchers("/insert").authenticated()
+				.antMatchers("/edit/*").authenticated()
+				.antMatchers("/post/postInsert").authenticated()
+				.antMatchers("/delete/*").authenticated()
+			).csrf(csrf -> csrf
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			).formLogin(login -> login
+					.loginProcessingUrl("/oauth/twitter/access")
+					.loginPage("/login")
+			).logout(logout -> logout
+					.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+					.logoutUrl("/logout")
+					.logoutSuccessUrl("/")
+					.deleteCookies("JSESSIONID").invalidateHttpSession(true)
+					.deleteCookies("autoLogin")
+			);
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:8080","http://localhost:8080","https://koalamania.herokuapp.com"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/api/users/**", configuration);
+		return source;
+	}
+
 }
