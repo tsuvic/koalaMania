2b50d437697716eef241ffb50b243f189b5dc753
tsuvi <admja11@gmail.com>
2022-07-18 15:46:17 +0900

    CORSエラーにより、マイページが表示されない事象を解消。

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -9,11 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -28,9 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-				.cors(cors -> cors
-						.configurationSource(corsConfigurationSource())
-				).authorizeRequests(authorize -> authorize
+				.authorizeRequests(authorize -> authorize
 						.antMatchers("/").permitAll()
 						.antMatchers("/insert").authenticated()
 						.antMatchers("/edit/*").authenticated()
@@ -56,18 +48,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	/* https://qiita.com/rubytomato@github/items/6c6318c948398fa62275#authenticationentrypoint */
 	AuthenticationEntryPoint authenticationEntryPoint() {
 		return new SimpleAuthenticationEntryPoint();
-	}
-
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://127.0.0.1:8080","http://localhost:8080","https://koalamania.herokuapp.com"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-		configuration.setAllowCredentials(true);
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/api/users/**", configuration);
-		return source;
 	}
 
 }
